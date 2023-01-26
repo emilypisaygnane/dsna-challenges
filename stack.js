dfbbea45f50eb8429ae4e56621874421de55c3a6
Emily Pisaygnane <episaygnane@me.com>
2023-01-26 12:01:50 -0800

    stack completed with tests

---
+++ stack.js
@@ -0,0 +1,47 @@
+class Stack {
+  constructor() {
+    this.stack = [];
+  }
+
+  //methods
+  push(element) {
+    this.stack.push(element);
+  }
+
+  pop() {
+    return this.stack.pop();
+  }
+
+  peek() {
+    return this.stack[this.stack.length - 1];
+  }
+
+  isEmpty() {
+    return this.stack.length === 0;
+  }
+
+  size() {
+    return this.stack.length;
+  }
+
+  clear() {
+    this.stack = [];
+  }
+  
+  print() {
+    console.log(this.stack.toString());
+  }
+}
+
+const stack = new Stack();
+stack.push('fox');
+stack.push('goose');
+stack.push('lizard');
+console.log(stack.pop()); // 'lizard'
+console.log(stack.peek()); // 'goose'
+console.log(stack.pop()); // 'goose'
+stack.push('llama');
+console.log(stack.pop()); // 'llama'
+console.log(stack.peek()); // 'fox'
+console.log(stack.pop()); // 'fox'
+console.log(stack.pop()); // null
