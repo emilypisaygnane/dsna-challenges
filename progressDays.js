fc7ea706a3be20e940d4430ea7158002508154bb
Emily Pisaygnane <episaygnane@me.com>
2023-01-29 15:45:08 -0800

    fixed solution for ProgressDays

--- progressDays.js
+++ progressDays.js
@@ -1,12 +1,15 @@
-const progressDays = (miles) => {
-  let progArr = [];
-  let progress = 0;
+function progressDays(miles) {
+  let count = 0
+
   for (let i = 0; i < miles.length; i++) {
-    if (miles[i] < miles[i + 1]) {
-      progress++;
-      progArr.push(progress);
-    }
-    return progArr;
+      if (miles[i] < miles [i + 1]) {
+          count++
+      }
   }
-};
-console.log(progressDays([3, 4, 1, 2]));
+
+  console.log(count)
+}
+
+progressDays([10, 11, 12, 9, 10]);
+progressDays([6, 5, 4, 3, 2, 9]);
+progressDays([9, 9]);
