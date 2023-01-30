bedf5141863241bb1c7af69ff78c13a4423bf8a1
Emily Pisaygnane <episaygnane@me.com>
2023-01-29 16:16:20 -0800

    add solution for puncuation challenge and created new file

---
+++ hof.js
@@ -0,0 +1,11 @@
+const addPunctuation = (punctuation) => {
+  return (str) => str + punctuation
+}
+
+const addExcitement = addPunctuation('!!!');
+console.log(addExcitement('Hello World'));
+console.log(addExcitement('Pokemon, catch em all'));
+
+const addUnsure = addPunctuation('?!?');
+console.log(addUnsure('Hello World'));
+console.log(addUnsure('Pokemon, catch em all'));
